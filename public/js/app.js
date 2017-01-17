6636400b934029c3e78965000d3e5db4aa841577
Bradley Anderson <bcaytonanderson@gmail.com>
2017-01-16 23:41:46 -0600

    Unit 5, pt 1: refactoring code. Organize code by creating a new module which groups similar directives, and list new module as a dependency of main module.

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,8 @@
 // wrap entire app in a closure
 (function() {
 	// module line: name ('store'), and dependencies array (currently empty)
-	var app = angular.module('store', []);
-
-	// the dash in the html tag will translate to camelCase in JS
-	app.directive('productTitle', function() {
-		// returns a directive definition object
-		return {
-			// configuration options
-			// specifies the type of directive; E stands for element here
-			restrict: 'E',
-			templateUrl: 'product-title.html'
-
-			// if we wanted to make it an attribute directive:
-			// restrict: 'A',
-			// templateUrl: 'product-title.html'
-		};
-
-	});
-
-	app.directive('productPanels', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'product-panels.html',
-
-			// to include the controller, pull the logic from the controller written earlier using the controller keyword
-			controller:function(){
-				this.tab = 1;
-
-				this.selectTab = function(setTab) {
-					this.tab = setTab;
-				};
-
-				this.isSelected = function(checkTab) {
-					return this.tab === checkTab;
-				};
-			},
-
-			// specify the controller's alias
-			controllerAs: 'panels'
-		};
-	});
+	// now that we have another module, we need to tell this one that it depends on the new one.
+	var app = angular.module('store', ['store-products']);
 
 	// good practice to initialize all variables
 	app.controller('ReviewController', function() {
