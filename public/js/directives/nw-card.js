9a185e5219422dcbf1fb6d97f3482263b160f510
Bradley Anderson <bcaytonanderson@gmail.com>
2017-01-18 18:06:16 -0600

    Unit 2, pt 2: Using the scope object to configure directives.

--- public/js/directives/nw-card.js
+++ public/js/directives/nw-card.js
@@ -1,5 +1,7 @@
 angular.module("NoteWrangler")
 .directive("nwCard", function() {
+	// to give each card a number, we can implement a num variable and increment it for each card
+	var num = 1;
 	return {
 		restrict: 'E',
 		templateUrl: "templates/directives/nw-card.html",
@@ -7,11 +9,24 @@ angular.module("NoteWrangler")
 		// 	this.header = "Note Title";
 		// },
 
+		// to isolate the scope, we can pass a scope key an empty object:
+		// scope: {},
+
 		// another way to implement the controller is to us $scope
-		controller: function($scope) {
-			$scope.header = "Note Title";
-		},
+		// controller: function($scope) {
+		// 	$scope.header = "Note Title" + num++;
+		// },
+
 		// using scope, we no longer need the controller alias
 		// controllerAs: "card"
+
+		// we need to tell our directive it might receive a header variable, and if so, to bind it to the scope:
+		scope: {
+			// this tells the directive we are passing in a string.
+			header: "@",
+
+			// the note cards display more than just headers, though. We can set the two-way binding for icon as "=" and pass in as many bindings as we want.
+			icon: "="
+		}
 	};
 });
