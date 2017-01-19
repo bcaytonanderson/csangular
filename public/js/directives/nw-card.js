28014b6f1a8accc1a0e7c3442ba0f1e827b46acd
Bradley Anderson <bcaytonanderson@gmail.com>
2017-01-18 18:37:07 -0600

    Unit 2, pt 3. Using the link function.

--- public/js/directives/nw-card.js
+++ public/js/directives/nw-card.js
@@ -3,6 +3,19 @@ angular.module("NoteWrangler")
 	// to give each card a number, we can implement a num variable and increment it for each card
 	var num = 1;
 	return {
+		// link's first two auto-injected parameters are scope and element: element refers to the outermost element of the included template (div class="card")
+
+		// link also has an attrs parameter which allows us to access any attrs set on the directive's element
+
+		link: function(scope, element, attrs) {
+			// this is where we can use the markdown library, but we have to tell angular that it can trust it to render the html properly using ngBindHtml and $sce
+			scope.description = $sce.trustAsHtml(markdown.toHTML(scope.description));
+			element.on("click", function() {
+				element.("div.card p").toggleClass("hidden");
+			});
+			// for debugging purposes, we can log an attr like so:
+			console.log(attrs.header);
+		},
 		restrict: 'E',
 		templateUrl: "templates/directives/nw-card.html",
 		// controller: function() {
@@ -23,10 +36,11 @@ angular.module("NoteWrangler")
 		// we need to tell our directive it might receive a header variable, and if so, to bind it to the scope:
 		scope: {
 			// this tells the directive we are passing in a string.
-			header: "@",
+			header: "=",
 
 			// the note cards display more than just headers, though. We can set the two-way binding for icon as "=" and pass in as many bindings as we want.
-			icon: "="
+			icon: "=",
+			description: "="
 		}
 	};
 });
